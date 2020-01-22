0cd17c509afeaa59b3bd1bab2b09d0dd420c93fb
Jonathan Higger <jonathan.higger@gmail.com>
2020-01-22 22:27:28 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -8,6 +8,6 @@ BASE_HOA = {
 def add_character(show, name)
   # Write your implementation here
   # Should return the array of the 'show' argument
-  BASE_HOA[show] = Array.concat(BASE_HOA[show],name).sort
+  BASE_HOA[show] = BASE_HOA[show].concat(name).sort
   return BASE_HOA[show]
 end
